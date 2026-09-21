cca686b04fd599c9ce5cb8e828962d0fc4afefaa
Filip Lajtar <lajtar.filip@gmail.com>
2026-09-21 19:11:44 +0200

    icons

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// IMPORTS
+
+import './style.css';
+import { gsap } from "gsap";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+// CANVAS
+
+const canvas = document.querySelector('canvas.webgl');
+
+// SCENE
+
+const scene = new THREE.Scene();
